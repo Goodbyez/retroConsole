0ea674bcf04f73cd2db87bc66bbdb48c5aec8a43
Goodbyez <miradadesigual@gmail.com>
2021-04-30 00:17:23 +0200

    Infructuosas modificaciones para obtener height de la Class del redBigButton, hay que seguir investigando

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 function pushButton(letter, value2){
     document.getElementById(letter).style.display ="none";
+    /*document.getElementById(letter).style.height ="2em";
+    document.getElementById(letter).style.width ="2em";*/
     let audio = document.getElementById(value2);
     audio.play();
 }
 function unPushButton(letter2){
-    document.getElementById(letter2).style.display = "block";
+    document.getElementById(letter2).style.display ="flex";
+    /*document.getElementById(letter2).style.height = "3.4em";
+    document.getElementById(letter2).style.width = "3.4em";*/
 }
+/*
+function getSizes(letter3){
+    let meh = document.getElementsByClassName(letter3)[0].height();
+    console.log(meh);
+}
+*/
 function pusher(elEvento) {
     var evento = window.event || elEvento;
-    if(evento.keyCode==79){pushButton('o', 'audioPush')}
+    if(evento.keyCode==79){/*getSizes('redBigButton')*/;pushButton('o', 'audioPush')}
     if(evento.keyCode==75){pushButton('k', 'audioPush')}
     if(evento.keyCode==87){pushButton('w', 'audioPush')}
     if(evento.keyCode==65){pushButton('a', 'audioPush')}
