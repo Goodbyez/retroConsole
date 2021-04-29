491ff675952af89e42740ddc738553f5c4678e30
Goodbyez <miradadesigual@gmail.com>
2021-04-29 18:05:13 +0200

    He cambiado la cruceta a una con todos los botones por separado y quitado los eventos onclick de los botones

--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ function pusher(elEvento) {
     var evento = window.event || elEvento;
     if(evento.keyCode==79){pushButton('o')}
     if(evento.keyCode==75){pushButton('k')}
+    if(evento.keyCode==87){pushButton('w')}
+    if(evento.keyCode==65){pushButton('a')}
+    if(evento.keyCode==83){pushButton('s')}
+    if(evento.keyCode==68){pushButton('d')}
 }
 function unPusher(elEvento2) {
     var evento = window.event || elEvento2;
     if(evento.keyCode==79){unPushButton('o')}
     if(evento.keyCode==75){unPushButton('k')}
+    if(evento.keyCode==87){unPushButton('w')}
+    if(evento.keyCode==65){unPushButton('a')}
+    if(evento.keyCode==83){unPushButton('s')}
+    if(evento.keyCode==68){unPushButton('d')}
 }
 
 function systemOn(){
