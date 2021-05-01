b6bc1ec8b698081f9788e07e157e30989131b511
Goodbyez <miradadesigual@gmail.com>
2021-05-02 00:47:37 +0200

    Mejora interactividad con el ratón en los botones y funcionalidad del sonido

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
-function pushButton(letter, valueSound, vHeigh, vWidth){
+function pushButton(letter, vHeigh, vWidth){
     document.getElementById(letter).style.height =vHeigh;
     document.getElementById(letter).style.width =vWidth;
-    let audio = document.getElementById(valueSound);
-    audio.play();
 }
 function unPushButton(letter2, vHeigh2, vWidth2){
     document.getElementById(letter2).style.height = vHeigh2;
     document.getElementById(letter2).style.width = vWidth2;
 }
-
-
+var sumare=0;
+function play(interp, idReference) {
+    let audio = document.getElementById(idReference);
+    if (interp > sumare){
+        audio.play();
+        sumare+=1;
+    } else {
+        audio.pause();
+        sumare-=1;
+    }
+}
+function playVFX(vfxRef){
+    let audio = document.getElementById(vfxRef);
+    audio.play();
+}
 
 function pusher(elEvento) {
     let buttonsOK = '2.6em';
     let buttonsWASD = '2.2em';
     let buttonWhiteH = '0.9em';
     let buttonWhiteW = '1.6em';
+    let soundWButton = "audioWPush";
+    let soundRButton = "audioPush";
     var evento = window.event || elEvento;
-    if(evento.keyCode==79){pushButton('o', 'audioPush', buttonsOK, buttonsOK)}
-    if(evento.keyCode==75){pushButton('k', 'audioPush', buttonsOK, buttonsOK)}
-    if(evento.keyCode==87){pushButton('w', 'audioPush', buttonsWASD, buttonsWASD)}
-    if(evento.keyCode==65){pushButton('a', 'audioPush', buttonsWASD, buttonsWASD)}
-    if(evento.keyCode==83){pushButton('s', 'audioPush', buttonsWASD, buttonsWASD)}
-    if(evento.keyCode==68){pushButton('d', 'audioPush', buttonsWASD, buttonsWASD)}
-    if(evento.keyCode==86){play(1, 'bsoMute'); pushButton('v', 'audioWPush', buttonWhiteH, buttonWhiteW)}
+    if(evento.keyCode==79){pushButton('o', buttonsOK, buttonsOK); playVFX(soundRButton)}
+    if(evento.keyCode==75){pushButton('k', buttonsOK, buttonsOK); playVFX(soundRButton)}
+    if(evento.keyCode==87){pushButton('w', buttonsWASD, buttonsWASD); playVFX(soundRButton)}
+    if(evento.keyCode==65){pushButton('a', buttonsWASD, buttonsWASD); playVFX(soundRButton)}
+    if(evento.keyCode==83){pushButton('s', buttonsWASD, buttonsWASD); playVFX(soundRButton)}
+    if(evento.keyCode==68){pushButton('d', buttonsWASD, buttonsWASD); playVFX(soundRButton)}
+    if(evento.keyCode==86){play(1, 'bsoMute'); pushButton('v', soundWButton, buttonWhiteH, buttonWhiteW)}
 }
 function unPusher(elEvento2) {
     var evento = window.event || elEvento2;
@@ -46,16 +59,6 @@ function systemOn(){
     document.onkeyup = unPusher;
 }
 
-var sumare=0;
-function play(interp, idReference) {
-    let audio = document.getElementById(idReference);
-    if (interp > sumare){
-        audio.play();
-        sumare+=1;
-    } else {
-        audio.pause();
-        sumare-=1;
-    }
-}
+
 
 systemOn();
